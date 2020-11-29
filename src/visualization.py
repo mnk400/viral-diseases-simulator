a19707a4f9156509239dc0cb6b97c7da2f113b30
Manik <mnk_400@icloud.com>
2020-11-29 00:39:32 -0500

    cleanup and a new experiments branch

--- src/visualization.py
+++ src/visualization.py
@@ -32,32 +32,9 @@ class Visualization():
             data2 = np.c_[self.putil.person.get_dataframe().loc[self.putil.person.get_all_infected()]['x_axis'],  self.putil.person.get_dataframe().loc[self.putil.person.get_all_infected()]['y_axis']]
             self.scat.set_offsets(data1)
             self.scat2.set_offsets(data2)
-        # self.scat.set_offsets([self.putil.person.get_dataframe().loc[self.putil.person.get_all_healthy()]['x_axis'], self.putil.person.get_dataframe().loc[self.putil.person.get_all_healthy()]['y_axis']])
-        #self.ax.set_offset(self.putil.person.get_dataframe().loc[self.putil.person.get_all_infected()]['x_axis'], self.putil.person.get_dataframe().loc[self.putil.person.get_all_infected()]['y_axis'], s = 10, color = 'red')
+
         return self.scat, self.scat2, 
 
-    # def animate(self):
-    #     fig = plt.figure(figsize=(10,10))
-    #     plt.xlim(self.putil.x_bounds[0] , self.putil.x_bounds[1])
-    #     plt.ylim(self.putil.y_bounds[0] , self.putil.y_bounds[1])
-    #     scatter1 = plt.scatter(self.putil.person.get_dataframe().loc[self.putil.person.get_all_healthy()]['x_axis'], self.putil.person.get_dataframe().loc[self.putil.person.get_all_healthy()]['y_axis'], s = 10, color = 'gray')
-    #     scatter2 = plt.scatter(self.putil.person.get_dataframe().loc[self.putil.person.get_all_infected()]['x_axis'], self.putil.person.get_dataframe().loc[self.putil.person.get_all_infected()]['y_axis'], s = 10, color = 'red')
-        
-    #     # def update(frame):
-    #     #     data1 = np.c_[self.putil.person.get_dataframe().loc[self.putil.person.get_all_healthy()]['x_axis'],  self.putil.person.get_dataframe().loc[self.putil.person.get_all_healthy()]['y_axis']]
-    #     #     scatter1.set_offsets(data1)
-    #     #     data2 = np.c_[self.putil.person.get_dataframe().loc[self.putil.person.get_all_infected()]['x_axis'],  self.putil.person.get_dataframe().loc[self.putil.person.get_all_infected()]['y_axis']]
-    #     #     scatter2.set_offsets(data2)
-    #     #     self.putil.move()
-    #     #     return scatter1, scatter2
-
-    #     ani = FuncAnimation(fig, func = update, frames=500, interval=33, blit=True)
-    #     # for i in range(15):
-    #     #     plt.clf()
-    #     #     self.update()
-    #     #     plt.pause(1)
-        
-    #     plt.show()
         
 
 if __name__ == "__main__":
