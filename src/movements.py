cbf10f7eb9b2a77a8cce495d27df883c505bacd3
Pallak <pallak.singh2409@gmail.com>
2020-11-29 15:49:25 -0500

    minor experiment on movements

--- src/movements.py
+++ src/movements.py
@@ -1,4 +1,9 @@
 '''
-file with classes and code which control how a particular person
+File with classes and code which control how a particular person
 will move and to where
 '''
+class Movement():
+    """
+    Class providing abstraction into each movement of the population
+    """    
+    
