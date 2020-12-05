90b3ea93a8bfa75c56914bcc643b0f2ab75f0e74
Manik <mnk_400@icloud.com>
2020-12-05 15:44:07 -0500

    trying out a fix for macOS

--- src/gui/buttons_frame.py
+++ src/gui/buttons_frame.py
@@ -42,47 +42,47 @@ class ButtonsFrame(ttk.Frame):
         label_frame.grid_propagate(0)
 
         label_frame_label2 = ttk.Label(text="Simulation Mode")
-        label_frame2 = ttk.LabelFrame(master=label_frame, labelwidget=label_frame_label2, height=self.height * 0.13,
-                                    width=self.width * 0.8)
-        label_frame2.grid(row=0, column=0, padx=self.width * 0.07)
+        label_frame2 = ttk.LabelFrame(master=label_frame, labelwidget=label_frame_label2, height=float(label_frame.winfo_reqheight()) * 0.145,
+                                    width=float(label_frame.winfo_reqwidth()) * 0.8)
+        label_frame2.grid(row=0, column=0, padx=float(label_frame.winfo_reqwidth()) * 0.07)
         label_frame2.grid_propagate(0)
 
         self.my_var = tk.IntVar()
         self.my_var.set(2)
         rb1 = ttk.Radiobutton(master=label_frame2, text='Render Mode', variable=self.my_var, value=1)
         rb2 = ttk.Radiobutton(master=label_frame2, text='Live Simulation', variable=self.my_var, value=2)
-        rb1.grid(row=0,column=0,columnspan=1, padx=self.width*0.10, pady=self.height*0.005, sticky=tk.W)
-        rb2.grid(row=1,column=0,columnspan=1, padx=self.width*0.10, pady=self.height*0.005, sticky=tk.W)
+        rb1.grid(row=0,column=0,columnspan=1, padx=float(label_frame.winfo_reqwidth()) * 0.10, pady=float(label_frame.winfo_reqheight())*0.005, sticky=tk.W)
+        rb2.grid(row=1,column=0,columnspan=1, padx=float(label_frame.winfo_reqwidth()) * 0.10, pady=float(label_frame.winfo_reqheight())*0.005, sticky=tk.W)
 
         label_frame_label3 = ttk.Label(text="Preventive Options")
-        label_frame3 = ttk.LabelFrame(master=label_frame, labelwidget=label_frame_label3, height=self.height * 0.13,
-                                    width=self.width * 0.8)
-        label_frame3.grid(row=1, column=0, padx=self.width * 0.07)
+        label_frame3 = ttk.LabelFrame(master=label_frame, labelwidget=label_frame_label3, height=float(label_frame.winfo_reqheight()) * 0.145,
+                                    width=float(label_frame.winfo_reqwidth()) * 0.8)
+        label_frame3.grid(row=1, column=0, padx=float(label_frame.winfo_reqwidth()) * 0.07)
         label_frame3.grid_propagate(0)
 
         #social_distance_enable = tk.IntVar()
         social_distance_check = ttk.Checkbutton(master=label_frame3, text='Enable Social Distancing')
         social_distance_check.invoke()
 
-        social_distance_check.grid(row=0,column=0,columnspan=1, padx=self.width*0.10, pady=self.height*0.005, sticky=tk.W)
+        social_distance_check.grid(row=0,column=0,columnspan=1, padx=float(label_frame.winfo_reqwidth())*0.10, pady=float(label_frame.winfo_reqheight())*0.005, sticky=tk.W)
 
         #mask_wearing_enable = tk.IntVar()
         mask_wearing_check = ttk.Checkbutton(master=label_frame3, text='Enable Mask Wearing')
 
-        mask_wearing_check.grid(row=1,column=0,columnspan=1, padx=self.width*0.10, pady=self.height*0.005, sticky=tk.W)
+        mask_wearing_check.grid(row=1,column=0,columnspan=1, padx=float(label_frame.winfo_reqwidth())*0.10, pady=float(label_frame.winfo_reqheight())*0.005, sticky=tk.W)
         mask_wearing_check.invoke()
 
         # Load Config Data button
         self.load_button = ttk.Button(master=label_frame, text="Start Covid Simulation", command="#", width=25)
-        self.load_button.grid(row=4,column=0,columnspan=1, padx=self.width*0.15, pady=self.height*0.02)
+        self.load_button.grid(row=4,column=0,columnspan=1, padx=float(label_frame.winfo_reqwidth())*0.15, pady=float(label_frame.winfo_reqheight())*0.02)
 
         # Load influenza data button
         self.load_inf_button = ttk.Button(master=label_frame, text="Start Influenza Simulation", command="#", width=25)       
-        self.load_inf_button.grid(row=5,column=0,columnspan=1, padx=self.width*0.15, pady=self.height*0.02)
+        self.load_inf_button.grid(row=5,column=0,columnspan=1, padx=float(label_frame.winfo_reqwidth())*0.15, pady=float(label_frame.winfo_reqheight())*0.02)
 
         #Start custom sim button
         self.start_sim_button = ttk.Button(master=label_frame, text="Start Custom Simulation", command='#', width=25)
-        self.start_sim_button.grid(row=6,column=0,columnspan=1, padx=self.width*0.15, pady=self.height*0.02)
+        self.start_sim_button.grid(row=6,column=0,columnspan=1, padx=float(label_frame.winfo_reqwidth())*0.15, pady=float(label_frame.winfo_reqheight())*0.02)
 
 
         
