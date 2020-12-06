37c66f52230de69ee2997ad98fae121c27c8b4d1
Yatish Pitta <pitta.y@northeastern.edu>
2020-12-06 18:18:49 -0500

    final virus util test

--- test/test_virus_util.py
+++ test/test_virus_util.py
@@ -1,16 +1,23 @@
+"""
+Test file for population util class
+
+Created on Dec 5th, 2020
+@author: Yatish Pitta
+"""
 import unittest
 from src.population import Population
 from config_util import ConfigUtil
 from virus_util import Virus
-from src.population_util import PopulationUtil
 import logging
-import functools
 import numpy as np
 import src.person_properties_util as index
 from src.movements import Movement
 
 
 class VirusUtilTest(unittest.TestCase):
+    """
+    Test case to test the Virus class
+    """
 
     def setUp(self) -> None:
         logging.basicConfig(format='%(asctime)s:%(module)s:%(levelname)s:%(message)s', level=logging.DEBUG)
@@ -26,7 +33,7 @@ class VirusUtilTest(unittest.TestCase):
         self.infection_range = self.config_util.getFloatValue("virus.stats", "infection_range")
         self.recovery_time = self.config_util.getFloatValue("virus.stats", "recovery_time")
         self.total_healthcare_capacity = self.size * (
-                    self.config_util.getIntegerValue("area.stats", "healthcare_capacity_ratio") / 100)
+                self.config_util.getIntegerValue("area.stats", "healthcare_capacity_ratio") / 100)
         self.mask_effectiveness = self.config_util.getDictionary("virus.stats", "mask_effectiveness")
         self.speed = self.config_util.getFloatValue("people.stats", "speed")
         self.x_bounds = [0, 1]
@@ -45,6 +52,10 @@ class VirusUtilTest(unittest.TestCase):
         self.movement = Movement()
 
     def test_infect(self):
+        """
+        Test case to test the infect method;
+        Compares the number of infected people before and after calling the infect method
+        """
         before_infect_population = self.population.get_all_infected()
         self.population.persons = self.movement.update_persons(self.population.persons, self.size, self.speed, 1)
         self.infected_person = np.random.randint(0, self.size)
@@ -64,10 +75,33 @@ class VirusUtilTest(unittest.TestCase):
 
             self.population = self.virus_util.infect(self.population, i)
 
-        self.assertIsInstance(self.population, Population)
         self.assertTrue(len(before_infect_population[:, 9]) != len(self.population.get_all_infected()[:, 9]) and len(
             self.population.get_all_infected()[:, 9]) > 1, "Test failed, infect did not work")
 
     def test_die_or_immune(self):
+        """
+        Test case to test the die_or_immune method
+        """
         dead_frame_1 = self.population.get_all_dead()
         self.assertEqual(len(dead_frame_1), 0)
+
+        self.population.persons = self.movement.update_persons(self.population.persons, self.size, self.speed, 1)
+        self.infected_person = np.random.randint(0, self.size)
+        self.population.persons[self.infected_person, index.g_value] = 3
+        self.population.set_infected_at(1, 0)
+        self.population.persons[self.infected_person, index.social_distance] = 0
+        self.population.persons[self.infected_person, 9] = 1
+        _xbounds = np.array([[0, 1]] * self.size)
+        _ybounds = np.array([[0, 1]] * self.size)
+
+        for i in range(1, self.size):
+            self.population.persons = self.movement.out_of_bounds(self.population.persons, _xbounds, _ybounds)
+
+            self.population.persons = self.movement.update_persons(self.population.persons, self.size, self.speed, 1)
+
+            self.population.persons = self.movement.update_pop(self.population.persons)
+
+            self.population = self.virus_util.infect(self.population, i)
+        self.population.persons[:, index.mortality_rate] = 1.00
+        self.virus_util.die_or_immune(self.population, int(self.population.get_all_infected()[0][0]))
+        self.assertNotEqual(len(self.population.get_all_dead()), 0)
